e3592d9de10b40b89c4de39875d3b0570cc8ef80
Haroon Khan <haroon.khan89041@gmail.com>
2026-08-17 16:31:36 -0700

    Add central school services export

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export * from "./studentService";
+export * from "./teacherService";
+export * from "./feeService";
+export * from "./attendanceService";
+export * from "./examService";
+export * from "./academicService";
+export * from "./schoolDataService";
