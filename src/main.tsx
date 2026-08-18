aebb6df1f16390776a787c84b7184b6913466406
Haroon Khan <haroon.khan89041@gmail.com>
2026-08-17 19:06:39 -0700

    Enable automatic dashboard totals and fee sync

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import AppAuth from "./AppAuth";
+import { DashboardLiveSync } from "./components/DashboardLiveSync";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppAuth />
+    <DashboardLiveSync />
   </StrictMode>,
 );
